673f7ee39d0ffc0d1e7673d3b63e4216eae6c9d8
Stuck <stuckf@dnb.com>
2021-11-22 11:23:49 -0500

    map to tomap

--- vpc.tf
+++ vpc.tf
@@ -24,16 +24,8 @@ resource "aws_vpc" "main_vpc" {
 
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidrs" {
   for_each   = {for cidr in var.vpc-cidrs : cidr => cidr 
-   cidr != var.vpc-cidrs[0]
+   if cidr != var.vpc-cidrs[0]
   }
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = each.value
 }
-
-
-
-
-
-
-
-  
