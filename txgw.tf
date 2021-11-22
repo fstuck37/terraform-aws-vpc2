49086738b8e67d57490ac1446f9d52ba7922cfdf
Stuck <stuckf@dnb.com>
2021-11-22 11:26:59 -0500

    map to tomap

--- txgw.tf
+++ txgw.tf
@@ -1,5 +1,7 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "txgw_attachment" {
-  count                   = var.transit_gateway_id == false ? 0 : 1
+  for_each   = {for vpc in [aws_vpc.main_vpc.id]: vpc => vpc
+   if var.transit_gateway_id
+  }
   subnet_ids              = local.map_subnet_id_list[element(var.subnet-order,1)]
   transit_gateway_id      = var.transit_gateway_id
   vpc_id                  = aws_vpc.main_vpc.id
@@ -12,3 +14,6 @@ resource "aws_route" "txgw-routes" {
   destination_cidr_block = each.value.route
   transit_gateway_id     = var.transit_gateway_id
 }
+
+
+
