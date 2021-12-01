9be5afeafd672edf8dbc62e8b0cad830699a1a86
fstuck37 <fstuck@gmail.com>
2021-12-01 07:01:19 -0500

    scratch

--- txgw.tf
+++ txgw.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_ec2_transit_gateway_vpc_attachment" "txgw_attachment" {
   for_each   = {for txgw in [var.transit_gateway_id]: txgw => txgw
    if var.transit_gateway_id != "null"
@@ -8,7 +9,6 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "txgw_attachment" {
   appliance_mode_support  = var.appliance_mode_support 
 }
 
-/*
 resource "aws_route" "txgw-routes" {
   for_each               = {for item in local.txgw_routes : item.name => item}
   route_table_id         = each.value.route_table
