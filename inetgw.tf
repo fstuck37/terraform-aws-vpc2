c4e6773d8afa2eda2a1de24832f2a9498250e4ed
fstuck37 <fstuck@gmail.com>
2021-12-02 07:48:25 -0500

    scratch

---
+++ inetgw.tf
@@ -0,0 +1,15 @@
+resource "aws_internet_gateway" "inet-gw" {
+  for_each = {for i in [format("%s", "${var.name-vars["account"]}-${var.name-vars["name"]}-${replace(var.region,"-", "")}-igw" )] : i=>i
+    if contains( keys(var.subnets), var.pub_layer)
+  }
+  vpc_id = aws_vpc.main_vpc.id
+  tags = merge(
+    var.tags,
+    tomap({ "Name" = each.value),
+    local.resource-tags["aws_internet_gateway"]
+  )
+}
+
+
+
+
