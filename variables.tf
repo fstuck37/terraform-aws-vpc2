bb53323e63c526d0ea8e5fff9d677f11a15ca8c8
fstuck37 <fstuck@gmail.com>
2021-12-02 17:28:11 -0500

    scratch

--- variables.tf
+++ variables.tf
@@ -257,15 +257,16 @@ variable "enable-dynamodb-endpoint" {
 
 
 variable "private_endpoints" {
-  description = "List of Maps for private AWS Endpoints Keys: name[Name of Resource IE: s3-endpoint], subnet[Name of the subnet group for the Endpoint IE: web], service[The Service IE: com.amazonaws.<REGION>.execute-api, <REGION> will be replaced with VPC Region], List of security_group IDs"
+  description = "List of Maps for private AWS Endpoints Keys: name[Name of Resource IE: s3-endpoint], subnet[Name of the subnet group for the Endpoint IE: web], service[The Service IE: com.amazonaws.<REGION>.execute-api, <REGION> will be replaced with VPC Region], List of security_group IDs, List of subnet layers to deploy interfaces to."
   type = list(object({
     name                = string
     subnet              = string
     service             = string
     security_groups     = list(string)
     private_dns_enabled = bool
+    subnets             = list(string)
   }))
-  default = {}
+  default = []
 }
 
 
