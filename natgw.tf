379175c98d67486ca93ee9cdf8c9ee6b34fd5f3f
fstuck37 <fstuck@gmail.com>
2021-12-02 14:31:10 -0500

    scratch

--- natgw.tf
+++ natgw.tf
@@ -2,7 +2,7 @@ resource "aws_nat_gateway" "natgw" {
   for_each = {for sd in local.subnet_data:sd.az=>sd
            if sd.layer == var.pub_layer && var.deploy_natgateways}
   allocation_id  = aws_eip.eip[each.value.az].id
-  subnet_id      = aws_subnet.subnets[each.key].id
+  subnet_id      = aws_subnet.subnets[each.value.name].id
   tags = {
     Name = each.value.name
   }
