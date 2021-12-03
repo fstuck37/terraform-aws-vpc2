8f44b9d5757becd4f04c4f4f105cb47d69d5c228
fstuck37 <fstuck@gmail.com>
2021-12-02 19:53:00 -0500

    scratch

--- eip.tf
+++ eip.tf
@@ -1,6 +1,6 @@
 resource "aws_eip" "eip" {
   for_each = {for sd in local.subnet_data:sd.az=>sd
-           if sd.layer == var.pub_layer && var.deploy_natgateways}
+           if sd.layer == var.pub_layer && var.deploy_natgateways }
   vpc = true
 }
 
