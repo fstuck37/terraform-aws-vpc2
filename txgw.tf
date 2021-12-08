c1e0d24c93276f976643de3a0f475089fb909dfb
Stuck <stuckf@dnb.com>
2021-12-08 00:12:51 -0500

    route53_reverse_zones

--- txgw.tf
+++ txgw.tf
@@ -2,7 +2,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "txgw_attachment" {
   for_each = { for txgw in [var.transit_gateway_id]: txgw => txgw
                if var.transit_gateway_id != "null" }
     subnet_ids              =  [for i in local.subnet_data : aws_subnet.subnets[i.name].id
-                                if i.layer != var.txgw_layer]
+                                if i.layer == var.txgw_layer]
     transit_gateway_id      = var.transit_gateway_id
     vpc_id                  = aws_vpc.main_vpc.id
     appliance_mode_support  = var.appliance_mode_support 
